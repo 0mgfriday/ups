a2d48e061b758c7ae21bfb3d7105017fb5fe5f6f
0mgfriday <0mgfriday@omg>
2023-06-04 21:53:22 -0400

    maven

--- bin/index.js
+++ bin/index.js
@@ -6,15 +6,16 @@ const search = "walmart";
 
 // await searchNpm(search)
 // await searchNuget(search)
+// await searchMaven(search)
 
 async function searchNpm(query) {
-    const response = await fetch('https://registry.npmjs.com/-/v1/search?text=' + encodeURIComponent(query));
+    const response = await fetch(`https://registry.npmjs.com/-/v1/search?text=${encodeURIComponent(query)}`);
     if (response.ok) {
         const data = await response.json();
     
         data.objects.forEach(obj => {
             console.log(obj.package.name)
-            console.log(obj.package.description)
+            console.log(obj.package.description ?? '[No description]')
             console.log(obj.package.links.npm)
             console.log()
         })
@@ -24,7 +25,7 @@ async function searchNpm(query) {
 }
 
 async function searchNuget(query) {
-    const response = await fetch('https://azuresearch-usnc.nuget.org/query?q=' + encodeURIComponent(query) + '&prerelease=true');
+    const response = await fetch(`https://azuresearch-usnc.nuget.org/query?q=${encodeURIComponent(query)}&prerelease=true`);
     if (response.ok) {
         const data = await response.json();
     
@@ -37,6 +38,22 @@ async function searchNuget(query) {
             console.log(obj.title)
             console.log(description)
             console.log(obj.registration)
+            console.log()
+        })
+    } else {
+        console.warn('npm search failed');
+    }
+}
+
+async function searchMaven(query) {
+    const response = await fetch(`https://search.maven.org/solrsearch/select?q=${encodeURIComponent(query)}&start=0&rows=20`);
+    if (response.ok) {
+        const data = await response.json();
+    
+        data.response.docs.forEach(obj => {
+            console.log(obj.id)
+            console.log('[No description]')
+            console.log(`https://search.maven.org/artifact/${obj.g}/${obj.a}`)
             console.log()
         })
     } else {
